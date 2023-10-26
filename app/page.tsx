81fc43233930ae11c44adfa3d2491b39fdae80c5
Viren <virenchauhan1100@gmail.com>
2023-10-26 21:49:00 +0530

    Bug Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import CarsFiltersOption from "@/components/Home/CarsFiltersOption";
 import CarsList from "@/components/Home/CarsList";
 import Hero from "@/components/Home/Hero";
@@ -8,54 +8,63 @@ import { BookCreatedFlagContext } from "@/context/BookCreatedFlagContext";
 import { getCarsList } from "@/services";
 import { useEffect, useState } from "react";
 
-
 export default function Home() {
-
-  const [carsList,setCarsList]=useState<any>([])
-  const [carsOrgList,setCarsOrgList]=useState<any>([])
-  const [showToastMsg,setShowToastMsg]=useState<boolean>(false);
-  useEffect(()=>{
+  const [carsList, setCarsList] = useState<any>([]);
+  const [carsOrgList, setCarsOrgList] = useState<any>([]);
+  const [showToastMsg, setShowToastMsg] = useState<boolean>(false);
+  useEffect(() => {
     getCarList_();
-  },[])
-  const getCarList_=async()=>{
-    const result:any=await getCarsList();
-    setCarsList(result?.carLists)
+  }, []);
+  const getCarList_ = async () => {
+    const result: any = await getCarsList();
+    setCarsList(result?.carLists);
     setCarsOrgList(result?.carLists);
-  }
+  };
 
-  const filterCarList=(brand:string)=>{
-    const filterList=carsOrgList.filter((item:any)=>
-    item.carBrand==brand);
-    setCarsList(filterList);
-  }
-
-  const orderCarList=(order:any)=>{
-    const sortedData = [...carsOrgList].sort((a, b) =>
-    order==-1? a.price - b.price:b.price - a.price);
-    setCarsList(sortedData);
-  }
-
-  useEffect(()=>{
-    if(showToastMsg)
-    {
-      setTimeout(function(){
-        setShowToastMsg(false)
-      },4000);
+  const filterCarList = (brand: string) => {
+    if (brand == "all") {
+      const filterList = carsOrgList.filter((item: any) => item);
+      setCarsList(filterList);
+    } else {
+      const filterList = carsOrgList.filter(
+        (item: any) => item.carBrand == brand
+      );
+      setCarsList(filterList);
     }
-  },[showToastMsg])
-  
+  };
+
+  const orderCarList = (order: any) => {
+    const sortedData = [...carsList].sort((a, b) =>
+      order == -1 ? a.price - b.price : b.price - a.price
+    );
+    setCarsList(sortedData);
+  };
+
+  useEffect(() => {
+    if (showToastMsg) {
+      setTimeout(function () {
+        setShowToastMsg(false);
+      }, 4000);
+    }
+  }, [showToastMsg]);
 
   return (
     <div className="p-5 sm:px-10 md:px-20">
-      <BookCreatedFlagContext.Provider value={{showToastMsg,setShowToastMsg}}>
-        <Hero/>
-        <SearchInput/>
-        <CarsFiltersOption carsList={carsOrgList}
-        orderCarList={(value:string)=>orderCarList(value)}
-        setBrand={(value:string)=>filterCarList(value)} />
+      <BookCreatedFlagContext.Provider
+        value={{ showToastMsg, setShowToastMsg }}
+      >
+        <Hero />
+        <SearchInput />
+        <CarsFiltersOption
+          carsList={carsOrgList}
+          orderCarList={(value: string) => orderCarList(value)}
+          setBrand={(value: string) => filterCarList(value)}
+        />
         <CarsList carsList={carsList} />
-        {showToastMsg?<ToastMsg msg={'Booking Created Successfully!'} />:null}
-    </BookCreatedFlagContext.Provider>
+        {showToastMsg ? (
+          <ToastMsg msg={"Booking Created Successfully!"} />
+        ) : null}
+      </BookCreatedFlagContext.Provider>
     </div>
-  )
+  );
 }
